7824020f3f56b78181f708ee464248959d608733
woohyunjng <woohyun.jng08@gmail.com>
2023-07-30 00:54:27 +0900

    feat(C/baekjoon): 1520 2098

---
+++ C/Baekjoon/.1000/1520.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int H[500][500], dp[500][500] = {}, checked[500][500] = {}, M, N;
+
+void dfs(int x, int y) {
+    int res = 0;
+    checked[x][y] = 1;
+
+    int go[4][2] = { { x + 1, y }, { x - 1, y }, { x, y + 1 }, { x, y - 1 } };
+    for (int i = 0 ; i < 4; i ++) {
+        if (go[i][0] < 0 || go[i][0] >= M || go[i][1] < 0 || go[i][1] >= N || checked[go[i][0]][go[i][1]])
+            continue;
+        if (H[go[i][0]][go[i][1]] >= H[x][y] || dp[go[i][0]][go[i][1]] == -1)
+            continue;
+
+        if (!dp[go[i][0]][go[i][1]])
+            dfs(go[i][0], go[i][1]);
+        if (dp[go[i][0]][go[i][1]] > 0)
+            res += dp[go[i][0]][go[i][1]];
+    }
+
+    checked[x][y] = 0;
+    if (res)
+        dp[x][y] = res;
+    else
+        dp[x][y] = -1;
+}
+
+int main() {
+    cin >> M >> N;
+    for (int i = 0 ; i < M ; i ++) {
+        for (int j = 0 ; j < N ; j ++) {
+            cin >> H[i][j];
+        }
+    }
+    dp[M - 1][N - 1] = 1;
+
+    dfs(0, 0);
+    cout << max(dp[0][0], 0);
+}
---
+++ C/Baekjoon/.2000/2098.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int dp[16][131072] = {}, W[16][16], N;
+
+bool check_vst(int vst, int i) {
+    int arr[17] = {}, j = 0;
+    while (vst) {
+        arr[j ++] = vst % 2;
+        vst = (int) (vst / 2);
+    }
+    return (bool) arr[i];
+}
+
+void dfs(int i, int vst) {
+    int res = 2147483646;
+    vst += (int) pow(2, i);
+
+    if (vst == pow(2, N) - 1) {
+        if (W[i][0])
+            dp[i][vst] = W[i][0];
+        else
+            dp[i][vst] = -1;
+        return;
+    }
+
+
+    for (int j = 0 ; j < N; j ++) {
+        if (check_vst(vst, j) || !W[i][j])
+            continue;
+
+        if (!dp[j][vst + (int) pow(2, j)]) {
+            dfs(j, vst);
+        }
+        if (dp[j][vst + (int) pow(2, j)] >= 0)
+            res = min(dp[j][vst + (int) pow(2, j)] + W[i][j], res);
+    }
+
+    if (res == 2147483646)
+        dp[i][vst] = -1;
+    else
+        dp[i][vst] = res;
+}
+
+int main() {
+    cin >> N;
+    for (int i = 0 ; i < N ; i ++) {
+        for (int j = 0 ; j < N ; j ++) {
+            cin >> W[i][j];
+        }
+    }
+
+
+    dfs(0, 0);
+    cout << dp[0][1];
+}
