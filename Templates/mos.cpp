c7a043c9b61f47fe15b071352c594d9994102ed4
woohyunjng <woohyun.jng08@gmail.com>
2024-06-20 12:31:26 +0900

    feat(templates): mos / smaller_to_larger templates

---
+++ Templates/mos.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define int long long
+#define MAX 100010
+
+using namespace std;
+typedef pair<int, int> pr;
+typedef array<int, 3> tp;
+
+int ans[MAX];
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, Q, S, A, B, nl, nr, l, r;
+    vector<tp> query;
+
+    cin >> N;
+    S = sqrt(N);
+
+    cin >> Q;
+    for (int i = 1; i <= Q; i++) {
+        cin >> A >> B;
+        query.push_back({A, B, i});
+    }
+
+    sort(query.begin(), query.end(), [S](tp a, tp b) {
+        int af = a[0] / S, bf = b[0] / S;
+        if (af == bf)
+            return a[1] / S < b[1] / S;
+        return af < bf;
+    });
+
+    for (int i = query[0][0]; i <= query[0][1]; i++)
+        continue;
+    ans[query[0][2]] = ans[0];
+    nl = query[0][0], nr = query[0][1];
+
+    for (int i = 1; i < M; i++) {
+        l = query[i][0], r = query[i][1];
+        while (nl < l)
+            continue;
+        while (nr > r)
+            continue;
+        while (nl > l)
+            continue;
+        while (nr < r)
+            continue;
+
+        ans[query[i][2]] = ans[0];
+    }
+
+    for (int i = 1; i <= Q; i++)
+        cout << ans[i] << '\n';
+
+    return 0;
+}
--- Templates/smaller_to_larger.cpp
+++ Templates/smaller_to_larger.cpp
@@ -6,13 +6,16 @@ using namespace std;
 typedef pair<int, int> pr;
 
 int uf_parent[MAX];
+set<int> S[MAX];
 
-int subst(int A, int B) { return A * MAX + B; }
-pr to_pair(int K) { return {K / MAX, K % MAX}; }
-
-void clear(int K) {
-    for (int i = 1; i <= K; i++)
+void clear(int K, bool set_clear = true) {
+    for (int i = 1; i <= K; i++) {
         uf_parent[i] = i;
+        if (set_clear) {
+            S[i].clear();
+            S[i].insert(i);
+        }
+    }
 }
 
 int find(int K) {
@@ -26,4 +29,9 @@ void uni(int A, int B) {
     if (A > B)
         swap(A, B);
     uf_parent[B] = A;
+
+    if (S[A].size() < S[B].size())
+        swap(S[A], S[B]);
+    for (int i : S[B])
+        S[A].insert(i);
 }
