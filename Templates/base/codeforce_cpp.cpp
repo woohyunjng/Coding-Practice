770d23edcc38213806a6e9575712bfa137932f98
woohyunjng <woohyun.jng08@gmail.com>
2024-07-18 23:25:44 +0900

    feat(templates): codeforces templates

---
+++ Templates/base/codeforce_cpp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+#define MAX 100100
+
+using namespace std;
+typedef pair<int, int> pr;
+typedef array<int, 3> tp;
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+    }
+
+    return 0;
+}
---
+++ Templates/base/codeforce_py.py
@@ -0,0 +1,7 @@
+from sys import stdin
+
+input = stdin.readline
+T = int(input())
+
+for t in range(T):
+    pass
