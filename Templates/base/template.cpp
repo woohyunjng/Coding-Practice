d86131a82ffa2f401740a489709df584a78120fa
woohyunjng <woohyun.jng08@gmail.com>
2024-07-22 20:37:04 +0900

    feat(templates): manacher

--- Templates/base/template.cpp
+++ Templates/base/template.cpp
@@ -8,8 +8,7 @@ typedef array<int, 3> tp;
 
 signed main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    cin.tie(0), cout.tie(0);
 
     return 0;
 }
---
+++ Templates/manacher.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+class Manacher {
+  public:
+    string S, K;
+    vector<int> rad;
+
+    Manacher(string S) : S(S) {
+        K = "#";
+        for (char i : S) {
+            K.push_back(i);
+            K.push_back('#');
+        }
+        rad.resize(K.size());
+    }
+
+    void build() {
+        int r = -1, c = -1;
+        for (int i = 0; i < K.size(); i++) {
+            if (i <= r)
+                rad[i] = min(r - i, rad[2 * c - i]);
+            while (i - rad[i] - 1 >= 0 && i + rad[i] + 1 < K.size() && K[i - rad[i] - 1] == K[i + rad[i] + 1])
+                rad[i]++;
+            if (r < i + rad[i]) {
+                r = i + rad[i];
+                c = i;
+            }
+        }
+    }
+};
