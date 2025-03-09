8c95cc9c6fb6f6a3325ca4e56652fb39e8681862
woohyunjng <woohyun.jng08@gmail.com>
2025-03-09 22:44:55 +0900

    feat(ps/jungol): 1554 4604 4936 5256 5755 7088

---
+++ PS/Jungol/5256.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+const int MAX = 200000;
+
+int N, A[MAX];
+vector<int> tree[MAX * 4];
+
+void init() {
+    for (int i = 1; i <= N; i++)
+        tree[i + N - 1].push_back(A[i]);
+
+    for (int i = N - 1; i > 0; i--) {
+        tree[i].resize(tree[i << 1].size() + tree[i << 1 | 1].size());
+        merge(tree[i << 1].begin(), tree[i << 1].end(), tree[i << 1 | 1].begin(), tree[i << 1 | 1].end(), tree[i].begin());
+    }
+}
+
+int query(int l, int r, int k) {
+    int res = 0;
+    for (l += N - 1, r += N; l < r; l >>= 1, r >>= 1) {
+        if (l & 1)
+            res += upper_bound(tree[l].begin(), tree[l].end(), k) - tree[l].begin(), l++;
+        if (r & 1)
+            --r, res += upper_bound(tree[r].begin(), tree[r].end(), k) - tree[r].begin();
+    }
+    return res;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int M, S, E, L, H;
+
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++)
+        cin >> A[i];
+
+    init();
+
+    for (int i = 1; i <= M; i++) {
+        cin >> S >> E >> L >> H;
+        cout << query(S, E, H) - query(S, E, L - 1) << '\n';
+    }
+
+    return 0;
+}
